486b5b23e2e1d7ebba3f7c56774ad57db7ec1f8a
langzhongyu <1837820987@qq.com>
2020-12-16 16:38:04 +0800

    'vue.config.js'

---
+++ vue.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  publicPath: './', // 根路径
+  devServer: {
+    open: true, // 浏览器自动打开页面
+    host: 'localhost',
+    port: 8080,
+    https: false,
+    proxy: {
+      // 配置跨域
+      '/api': {
+        target: 'http://ttapi.research.itcast.cn',
+        ws: true, // 是否跨域
+        changeOrigin: true,
+        // 重写地址
+        pathRewrite: {
+          '^/api': ''
+        }
+      }
+    }
+  }
+}

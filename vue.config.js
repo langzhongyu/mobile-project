95631aec95406cc3277598aed9a8dd521182474c
langzhongyu <1837820987@qq.com>
2020-12-16 17:36:47 +0800

    'config'

--- vue.config.js
+++ vue.config.js
@@ -1,21 +1,3 @@
-module.exports = {
-  publicPath: './', // 根路径
-  devServer: {
-    open: true, // 浏览器自动打开页面
-    host: 'localhost',
-    port: 8080,
-    https: false,
-    proxy: {
-      // 配置跨域
-      '/api': {
-        target: 'http://ttapi.research.itcast.cn',
-        ws: true, // 是否跨域
-        changeOrigin: true,
-        // 重写地址
-        pathRewrite: {
-          '^/api': ''
-        }
-      }
-    }
-  }
+module.exports={
+　　publicPath:process.env.NODE_ENV==='production'?'/mobile-project/':'/'
 }
